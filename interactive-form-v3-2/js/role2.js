8a41d2aafc7d2925fc5aa752fbdc91ee19adc802
gatadev <119637143+gatadev@users.noreply.github.com>
2023-05-06 10:13:21 -0400

    Update role2.js

--- interactive-form-v3-2/js/role2.js
+++ interactive-form-v3-2/js/role2.js
@@ -1,9 +1,10 @@
 /***Select the other job role so we can hide***/
  var secondJobRole = document.getElementById('other-job-role');
- 
+  var secondJobRoleValue = document.getElementById('other-job-role').value;
  // creating function to hide other-job-role
 
  var hideOtherJobByDefault = function(){
     return secondJobRole.style.display='none';
  }
 hideOtherJobByDefault();
+//creating a function to show the value of hideOtherJobRoleBydefault
